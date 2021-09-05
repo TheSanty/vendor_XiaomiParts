eeebfdb0c603c726e80f6def87531978086b1554
TheSanty <sudhiryadav.igi@gmail.com>
2021-09-06 04:16:18 +0530

    XiaomiParts: Remove Target Device Rule
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,1 @@
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),lavender))
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+include $(call all-subdir-makefiles) 
